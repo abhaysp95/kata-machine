6dba4ddee737d8c6815f553a5a0b1b415fc5cdcd
abhay <abhaysp95@protonmail.com>
2023-04-16 00:10:18 +0530

    feat: yes, it's bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,15 @@
 export default function bubble_sort(arr: number[]): void {
 
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			// at the end of each iteration, there will be one (extra) sorted
+			// element at the end of array
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
 }
